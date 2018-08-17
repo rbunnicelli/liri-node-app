ee7735fe527cba7cd7aa6f8f30096f86eccfe6c5
Machoman <rbunnicelli2010@yahoo.com>
2018-08-16 20:43:08 -0400

    .

--- liri.js
+++ liri.js
@@ -1,1 +1,9 @@
 require("dotenv").config();
+
+var twitter = require('twitter');
+var spotify = require('node-spotify-api');
+var request = require("request");
+var fs = require("fs");
+
+var spotifyKeys = new Spotify(keys.spotify);
+var twitterKeys = new Twitter(keys.twitter);
